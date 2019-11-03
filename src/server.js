d48e5bac362f54db7b01d8b0d4e6ab3672998430
Dilip Kr. Shukla <fusionstrings@gmail.com>
2019-11-03 22:51:13 +0100

    use now builder

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 import sirv from 'sirv';
 import polka from 'polka';
+import bodyParser from 'body-parser';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
-
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
+const app = polka();
+app.use( // You can also use Express
+	bodyParser.json(),
+	compression({ threshold: 0 }),
+	sirv('static', { dev, dotfiles: true }),
+	sapper.middleware()
+)
 	.listen(PORT, err => {
 		if (err) console.log('error', err);
 	});
+
+export default app.handler;
