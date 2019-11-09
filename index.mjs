3098ba8016c95b0eaf21f7ef162af3e9ebbf55b8
Dilip Kr. Shukla <fusionstrings@gmail.com>
2019-11-09 21:09:22 +0100

    export Card

--- index.mjs
+++ index.mjs
@@ -10,6 +10,7 @@ export {
 } from '@fusionstrings/elevate';
 
 export { default as Logo } from './src/components/logo.svelte';
+export { default as Card } from './src/components/card.svelte';
 export {default as BitmexSymbolList} from './src/components/bitmex-symbol-list.svelte'
 export { default as CardSymbolGraph } from './src/components/card-symbol-graph.svelte';
 export { default as StripNav } from './src/components/strip-nav.svelte';
