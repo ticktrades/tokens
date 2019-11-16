699dadbf285024e156f22907e7abc67dc808ed83
Dilip Kr. Shukla <fusionstrings@gmail.com>
2019-11-16 10:26:24 +0100

    update logo

--- src/components/logo.svelte
+++ src/components/logo.svelte
@@ -1,72 +1,83 @@
 <script>
-  export let segment;
+	export let segment;
 </script>
 
 <style>
-  #logo {
-    margin: 0;
-    padding: 0;
-    outline: 0;
-    border: none;
-    font-size: 2.8em;
-    font-weight: 700;
-    color: var(--brand-color);
-    text-align: center;
-    letter-spacing: -0.2rem;
-    position: absolute;
-    left: 20px;
-    top: 30px;
-    line-height: 0;
-  }
-  a {
-    margin: 0;
-    padding: 0;
-    outline: 0;
-    border: none;
-    color: var(--brand-color);
-    text-decoration: none;
-    height: 30px;
-    width: 26px;
-    display: inline-block;
-    transition: all 0.5s ease-in-out;
-  }
+	#logo {
+		margin: 0;
+		padding: 0;
+		outline: 0;
+		border: none;
+		font-size: 2.8em;
+		font-weight: 700;
+		color: var(--brand-color);
+		text-align: center;
+		letter-spacing: -0.2rem;
+		position: absolute;
+		left: 20px;
+		top: 30px;
+		line-height: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 42px;
+		height: 42px;
+		background: var(--color-darkest);
+	}
+	a {
+		margin: 0;
+		padding: 0;
+		outline: 0;
+		border: none;
+		color: var(--brand-color);
+		text-decoration: none;
+		height: 30px;
+		width: 26px;
+		display: inline-block;
+		transition: all 0.5s ease-in-out;
+	}
 </style>
 
 <h1 id="logo">
-  <a href=".">
+	<a href=".">
 
-    <svg
-      width="100%"
-      height="100%"
-      viewBox="0 0 275 238"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink">
-      <g
-        id="Page-1"
-        stroke="none"
-        stroke-width="1"
-        fill="none"
-        fill-rule="evenodd">
-        <g
-          id="xxxhdpi/large-ticktrades-small"
-          transform="translate(-111.000000, -132.000000)"
-          fill="gold">
-          <path
-            d="M383.916219,185.124715 L344.141642,185.124715
-            L344.141642,288.452148 C344.141642,297.24295 345.726845,303.367597
-            348.897298,306.826274 C352.067751,310.28495 358.336508,312.014262
-            367.703756,312.014262 C374.909331,312.014262 380.961923,311.581934
-            385.861715,310.717265 L385.861715,362.813314 C372.747568,366.992547
-            359.05718,369.082133 344.790141,369.082133 C319.714739,369.082133
-            301.160661,363.137623 289.12735,351.248423 C277.094039,339.359224
-            271.077474,321.453638 271.077474,297.531128 L271.077474,185.124715
-            L224.169413,185.124715 L224.169413,366.704305 L151.321411,366.704305
-            L151.321411,185.124715 L111.979167,185.124715 L111.979167,132.8125
-            L383.916219,132.8125 L383.916219,185.124715 Z"
-            id="π" />
-        </g>
-      </g>
-    </svg>
-  </a>
+		<svg
+			width="100%"
+			height="100%"
+			viewBox="0 0 275 238"
+			version="1.1"
+			xmlns="http://www.w3.org/2000/svg"
+			xmlns:xlink="http://www.w3.org/1999/xlink">
+			<g
+				id="Page-1"
+				stroke="none"
+				stroke-width="1"
+				fill="none"
+				fill-rule="evenodd">
+				<g
+					id="xxxhdpi/large-ticktrades-small"
+					transform="translate(-111.000000, -132.000000)"
+					fill="gold">
+					<path
+						d="M383.916219,185.124715 L344.141642,185.124715
+						L344.141642,288.452148 C344.141642,297.24295
+						345.726845,303.367597 348.897298,306.826274
+						C352.067751,310.28495 358.336508,312.014262
+						367.703756,312.014262 C374.909331,312.014262
+						380.961923,311.581934 385.861715,310.717265
+						L385.861715,362.813314 C372.747568,366.992547
+						359.05718,369.082133 344.790141,369.082133
+						C319.714739,369.082133 301.160661,363.137623
+						289.12735,351.248423 C277.094039,339.359224
+						271.077474,321.453638 271.077474,297.531128
+						L271.077474,185.124715 L224.169413,185.124715
+						L224.169413,366.704305 L151.321411,366.704305
+						L151.321411,185.124715 L111.979167,185.124715
+						L111.979167,132.8125 L383.916219,132.8125
+						L383.916219,185.124715 Z"
+						id="π" />
+				</g>
+			</g>
+		</svg>
+	</a>
 </h1>
