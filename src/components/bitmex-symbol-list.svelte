2361461a4d15ad879eca87bb031886cd21ca47e1
Dilip Kr. Shukla <contact@fusionstrings.com>
2019-12-05 23:09:10 +0100

    update price

--- src/components/bitmex-symbol-list.svelte
+++ src/components/bitmex-symbol-list.svelte
@@ -3,6 +3,7 @@
 
 	export let active = '';
 	export let symbolSummaries = [];
+	export let sortedActiveInstrumentTradeStore = {};
 </script>
 
 <style>
@@ -15,20 +16,37 @@
 	:global(.symbol-list > ul a) {
 		display: block;
 	}
-	:global(.symbol-list .active .button.invert){
+	:global(.symbol-list .active .button.invert) {
 		border-color: var(--brand-color);
 		color: var(--brand-color);
 	}
+	:global(.symbol-list .button.invert) {
+		display: flex;
+		justify-content: space-between;
+	}
+	.left{
+		text-align: left;
+		display: inline-block;
+	}
+	.number{
+		font-weight: 300;
+	}
 </style>
+{@debug sortedActiveInstrumentTradeStore}
 
 {#if symbolSummaries.length > 0}
 	<aside class="symbol-list">
 		<ul>
-			{#each symbolSummaries as symbolSummary}
+			{#each symbolSummaries as { symbol, href, price }}
 				<li
-					class:active={symbolSummary.symbol.toLocaleLowerCase() === active.toLowerCase()}>
-					<Button invert href={symbolSummary.href}>
-						{symbolSummary.symbol}
+					class:active={symbol.toLocaleLowerCase() === active.toLowerCase()}>
+					<Button invert {href}>
+						<span class="left">{symbol}</span>
+						<span class={`number ${sortedActiveInstrumentTradeStore[symbol] && sortedActiveInstrumentTradeStore[symbol].side ? sortedActiveInstrumentTradeStore[symbol].side.toLowerCase() : ''}`}>
+							{#if sortedActiveInstrumentTradeStore[symbol]}
+								{sortedActiveInstrumentTradeStore[symbol].price}
+							{:else}{price}{/if}
+						</span>
 					</Button>
 				</li>
 			{/each}
